f3bf45457f027247b08573495999a8f66a1b8fcc
Tra Nguyen <trant@icts.vn>
2017-07-28 17:04:32 +0700

    add put todos route

--- app.js
+++ app.js
@@ -89,6 +89,34 @@ app.delete('/todos/:id', function(req, res){
     }
 })
 
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10); //get ID and converse to integer
+    var matchedTodo = _.findWhere(todos, {id: todoId}); //findwhere(mảng, thuộc tính): Tìm qua danh sách (mảng) và trả về giá trị đầu tiên phù hợp với tất cả các cặp khóa-giá trị được liệt kê trong thuộc tính.
+    var body = _.pick(req.body, 'description', 'completed'); //chon lay gia tri des va completed trong req.body
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){ //kiem tra completed
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+})
+
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + '!');
 });
