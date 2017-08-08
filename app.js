ee7295c3538bbece7e2139c00295002199608909
Tra Nguyen <nguyenthanhtra.ptit@gmail.com>
2017-08-08 12:06:01 +0700

    convert DELETED with sequelize

--- app.js
+++ app.js
@@ -132,14 +132,33 @@ app.post('/todos', function(req, res){
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if (!matchedTodo){
-        res.status(404).json({"error": "no todo found with that id"});
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
+db.todo.destroy({
+    where: {
+        id: todoId
     }
+}).then(function(rowsDeleted){
+    if (rowsDeleted === 0){
+        res.status(404).json({
+            error: 'no todo with id'
+        });
+    } else {
+        res.status(202).send();
+    }
+}, function(e){
+    res.status(500).send();
+})
+
+    // var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    // if (!matchedTodo){
+    //     res.status(404).json({
+    //         "error": "no todo found with that id"
+    //     });
+    // } else {
+    //     todos = _.without(todos, matchedTodo);
+    //     res.json(matchedTodo);
+    // }
 })
 
 //PUT /todos/:id
